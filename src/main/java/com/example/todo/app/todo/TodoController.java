ec8aae23d4fa3518f78d49ba301a3bd8c39f2b13
takaaki <takaaking26@gmail.com>
2023-01-12 00:18:15 +0900

    20230111更新分

---
+++ src/main/java/com/example/todo/app/todo/TodoController.java
@@ -0,0 +1,63 @@
+package com.example.todo.app.todo;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+
+import com.github.dozermapper.core.Mapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import org.terasoluna.gfw.common.exception.BusinessException;
+import org.terasoluna.gfw.common.message.ResultMessage;
+import org.terasoluna.gfw.common.message.ResultMessages;
+
+import com.example.todo.domain.model.Todo;
+import com.example.todo.domain.service.todo.TodoService;
+
+@Controller
+@RequestMapping("todo")
+public class TodoController {
+    @Inject
+    TodoService todoService;
+
+    @Inject
+    Mapper beanMapper;
+
+    @ModelAttribute
+    public TodoForm setUpForm(){
+        TodoForm form = new TodoForm();
+        return form;
+    }
+
+    @GetMapping("list")
+    public String list(Model model){
+        Collection<Todo> todos = todoService.findAll();
+        model.addAttribute("todos", todos);
+        return "todo/list";
+    }
+
+    @PostMapping("create")
+    public String create(@Valid TodoForm todoForm, BindingResult bindingResult, Model model, RedirectAttributes attributes) {
+        if(bindingResult.hasErrors()){
+            return list(model);
+        }
+        Todo todo = beanMapper.map(todoForm, Todo.class);
+
+        try{
+            todoService.create(todo);
+        } catch (BusinessException e){
+            model.addAttribute(e.getResultMessages());
+            return list(model);
+        }
+        attributes.addFlashAttribute(ResultMessages.success().add(ResultMessage.fromText("Created successfully!")));
+            return "redirect:/todo/list";
+        }
+    }
