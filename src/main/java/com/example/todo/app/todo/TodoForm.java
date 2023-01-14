9d66790163b116171c326a7d6533ab94dfe6fcaf
takaaki <takaaking26@gmail.com>
2023-01-15 00:45:15 +0900

    20230111更新分

--- src/main/java/com/example/todo/app/todo/TodoForm.java
+++ src/main/java/com/example/todo/app/todo/TodoForm.java
@@ -1,22 +1,41 @@
 package com.example.todo.app.todo;
 
-import java.io.Serializable;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 
-public class TodoForm implements Serializable{
+public class TodoForm implements Serializable {
+    public interface TodoCreate {
+    }
+
+    public interface TodoFinish {
+    }
+
+    public interface TodoDelete {
+    }
+
     private static final long serialVersionUID = 1L;
 
-    @NotNull
-    @Size(min = 1, max = 30)
+    @NotNull(groups = {TodoFinish.class, TodoDelete.class})
+    private String todoId;
+
+    @NotNull(groups = {TodoCreate.class})
+    @Size(min = 1, max = 30, groups = {TodoCreate.class})
     private String todoTitle;
 
-    public String getTodoTitle(){
+    public String getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoTitle() {
         return todoTitle;
     }
 
-    public void setTodoTitle(String todoTitle){
-        this.todoTitle =todoTitle;
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
     }
 }
